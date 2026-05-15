7bb76fc862a74b30968796dd049c9bc763d76cf8
sivawarieth97 <sivawarieth97@gmail.com>
2026-05-15 14:38:14 +0530

    Fix tag name

--- Casks/stretch.rb
+++ Casks/stretch.rb
@@ -3,12 +3,11 @@ cask "stretch" do
 
   if Hardware::CPU.intel?
     sha256 "b6b5d95321bbc84bf87ff7c1604066962fcbb7e7e34d38dcccf5696b79c8d846"
-    url "https://github.com/praveensankar969/stretch/releases/download/v#{version}/Stretch-#{version}-x64.dmg"
+    url "https://github.com/praveensankar969/stretch/releases/download/latest/Stretch-#{version}-x64.dmg"
   else
     sha256 "67404871119505e3ef675504ac0d9c9e3235340b72335eb0c85ececc004f3cb4"
-    url "https://github.com/praveensankar969/stretch/releases/download/v#{version}/Stretch-#{version}-arm64.dmg"
+    url "https://github.com/praveensankar969/stretch/releases/download/latest/Stretch-#{version}-arm64.dmg"
   end
-
   name "Stretch"
   desc "A calm, local-only stretch reminder"
   homepage "https://github.com/praveensankar969/Stretch"
